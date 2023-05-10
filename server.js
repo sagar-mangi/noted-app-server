45e3fffed2f1ea7199964d6eda75836199357bc2
sagar-mangi <133117060+sagar-mangi@users.noreply.github.com>
2023-05-10 17:18:56 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./Routes/AuthRoutes");
+
+const app = express();
+
+app.listen(8080, (err)=> {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Server started on Port 8080");
+    }
+});
+
+mongoose.set("strictQuery", false);
+mongoose
+    .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("DB connected successfully");
+    })
+    .catch((err) => {
+        console.log(err.message);
+});
+
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "DELETE"],
+    credentials: true
+}))
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.use("/", authRoutes);
