7b6e3b74de383cac659629f79ed95a61a352efdb
sagar-mangi <133117060+sagar-mangi@users.noreply.github.com>
2023-05-10 17:17:44 +0500

    Create UserModel.js

---
+++ Models/UserModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const noteSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    notes: [noteSchema]
+})
+
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error("incorrect password");
+  }
+  throw Error("incorrect email");
+};
+
+module.exports = mongoose.model("Users", userSchema);
