851c796e4c69aa84af156fa33aa63a3011b8d6a2
sagar-mangi <133117060+sagar-mangi@users.noreply.github.com>
2023-05-31 03:20:32 +0500

    Update AuthMiddlewares.js

--- Middlewares/AuthMiddlewares.js
+++ Middlewares/AuthMiddlewares.js
@@ -77,24 +77,31 @@ module.exports.deleteNotes = (req, res, next) => {
 
 module.exports.editNotes = async (req, res, next) => {
   const token = req.cookies.jwt;
-  const title = req.body.title;
-  const content = req.body.content;
+  const prevNote = req.body.prevNote;
+  const newTitle = req.body.title;
+  const newContent = req.body.content;
 
   if (token) {
     try {
       const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
       const foundUser = await User.findOne({ _id: decodedToken.id });
+        
+      // Find the note document by its ID
+      const foundNote = await Note.findOne({ _id: prevNote._id });
 
-      // Create a new note document
-      const newNote = new Note({
-        title,
-        content,
-      });
-      await newNote.save();
+      if (!foundNote) {
+        return res.status(404).json({ error: 'Note not found' });
+      }
+       
+      // Check if the note belongs to the user
+      if (!foundUser.notes.includes(foundNote._id)) {
+        return res.status(403).json({ error: 'Unauthorized to edit this note' });
+      }
 
-      // Update the user's notes array with the note's ObjectId
-      foundUser.notes.push(newNote._id);
-      await foundUser.save();
+      // Update the note document with the new title and content
+      foundNote.title = newTitle || prevNote.title;
+      foundNote.content = newContent || prevNote.content;
+      await foundNote.save();
         
       // Populate the notes array with the actual note documents
       const populatedUser = await User.findOne({ _id: decodedToken.id }).populate('notes');
