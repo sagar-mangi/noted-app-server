3a313b9b54ab6ef990ae52004b842f0334866e9c
sagar-mangi <133117060+sagar-mangi@users.noreply.github.com>
2023-05-31 02:10:05 +0500

    Update AuthMiddlewares.js

--- Middlewares/AuthMiddlewares.js
+++ Middlewares/AuthMiddlewares.js
@@ -75,41 +75,32 @@ module.exports.deleteNotes = (req, res, next) => {
     }
 };
 
-module.exports.editNotes = async (req, res, next) => {
+module.exports.editNotes = (req, res, next) => {
   const token = req.cookies.jwt;
-  const prevNote = req.body.prevNote;
-  const newTitle = req.body.title;
-  const newContent = req.body.content;
+  const title = req.body.title;
+  const content = req.body.content;
 
   if (token) {
     try {
       const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
       const foundUser = await User.findOne({ _id: decodedToken.id });
 
-      // Find the note document by its ID
-      const foundNote = await Note.findOne({ _id: prevNote._id });
+      // Create a new note document
+      const newNote = new Note({
+        title,
+        content,
+      });
+      await newNote.save();
 
-      if (!foundNote) {
-        return res.status(404).json({ error: 'Note not found' });
-      }
-
-      // Check if the note belongs to the user
-      if (!foundUser.notes.includes(foundNote._id)) {
-        return res.status(403).json({ error: 'Unauthorized to edit this note' });
-      }
-
-      // Update the note document with the new title and content
-      foundNote.title = newTitle || prevNote.title;
-      foundNote.content = newContent || prevNote.content;
-      await foundNote.save();
-
-      // Populate the updated notes array
+      // Update the user's notes array with the note's ObjectId
+      foundUser.notes.push(newNote._id);
+      await foundUser.save();
+        
+      // Populate the notes array with the actual note documents
       const populatedUser = await User.findOne({ _id: decodedToken.id }).populate('notes');
       res.json({ notes: populatedUser.notes });
     } catch (err) {
-      res.status(500).json({ error: 'Internal server error' });
+      res.json({ err });
     }
-  } else {
-    res.status(401).json({ error: 'Unauthorized' });
   }
-};
+}
