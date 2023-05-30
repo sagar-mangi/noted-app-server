aa66994daed1eefe49165e72ca7c945c63fe0f7a
sagar-mangi <133117060+sagar-mangi@users.noreply.github.com>
2023-05-31 04:47:06 +0500

    Update AuthMiddlewares.js

--- Middlewares/AuthMiddlewares.js
+++ Middlewares/AuthMiddlewares.js
@@ -76,40 +76,40 @@ module.exports.deleteNotes = (req, res, next) => {
 };
 
 module.exports.editNotes = async (req, res, next) => {
-//   const token = req.cookies.jwt;
-//   const prevNote = req.body.prevNote;
-//   const newTitle = req.body.title;
-//   const newContent = req.body.content;
+  const token = req.cookies.jwt;
+  const prevNote = req.body.prevNote;
+  const newTitle = req.body.title;
+  const newContent = req.body.content;
     console.log("prevNote")
     console.log("newTitle")
 
-//   if (token) {
-//     try {
-//       const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-//       const foundUser = await User.findOne({ _id: decodedToken.id });
+  if (token) {
+    try {
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+      const foundUser = await User.findOne({ _id: decodedToken.id });
         
-//       // Find the note document by its ID
-//       const foundNote = await Note.findOne({ _id: prevNote.id });
+      // Find the note document by its ID
+      const foundNote = await Note.findOne({ _id: prevNote.id });
 
-//       if (!foundNote) {
-//         return res.status(404).json({ error: 'Note not found' });
-//       }
+      if (!foundNote) {
+        return res.status(404).json({ error: 'Note not found' });
+      }
        
-//       // Check if the note belongs to the user
-//       if (!foundUser.notes.includes(foundNote._id)) {
-//         return res.status(403).json({ error: 'Unauthorized to edit this note' });
-//       }
+      // Check if the note belongs to the user
+      if (!foundUser.notes.includes(foundNote._id)) {
+        return res.status(403).json({ error: 'Unauthorized to edit this note' });
+      }
 
-//       // Update the note document with the new title and content
-//       foundNote.title = newTitle || prevNote.title;
-//       foundNote.content = newContent || prevNote.content;
-//       await foundNote.save();
+      // Update the note document with the new title and content
+      foundNote.title = newTitle || prevNote.title;
+      foundNote.content = newContent || prevNote.content;
+      await foundNote.save();
         
-//       // Populate the notes array with the actual note documents
-//       const populatedUser = await User.findOne({ _id: decodedToken.id }).populate('notes');
-//       res.json({ notes: populatedUser.notes });
-//     } catch (err) {
-//       res.json({ err });
-//     }
-//   }
+      // Populate the notes array with the actual note documents
+      const populatedUser = await User.findOne({ _id: decodedToken.id }).populate('notes');
+      res.json({ notes: populatedUser.notes });
+    } catch (err) {
+      res.json({ err });
+    }
+  }
 }
