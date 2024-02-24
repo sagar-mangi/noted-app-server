b08ca6af7d7fd79870929ae0d50ca57e806c65e5
sagar-mangi <133117060+sagar-mangi@users.noreply.github.com>
2024-02-25 01:34:28 +0500

    Update AuthMiddlewares.js

--- Middlewares/AuthMiddlewares.js
+++ Middlewares/AuthMiddlewares.js
@@ -78,8 +78,8 @@ module.exports.deleteNotes = (req, res, next) => {
 module.exports.editNote = (req, res, next) => {
     const token = req.cookies.jwt;
     const {title, content, id} = req.body;
-    console.log(id)
-    console.log(token)
+
+    
     if (token) {
         jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
             if (err) {
